0d9bca130eb1a07e0118afdf9b686a23f5885f19
Pierre-Louis <pierre-louis.landouzi@epita.fr>
2021-07-09 19:41:42 +0200

    presentation

---
+++ helloWorld/src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    public void printHelloWorld() {
+        System.out.print("Hello World!");
+    }
+
+    static void printHelloWorldErr() {
+        System.err.println("Hello World!");
+    }
+}
