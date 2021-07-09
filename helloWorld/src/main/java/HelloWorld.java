a3eae3fc2692f8fd3f231e3a50321876b9150452
Pierre-Louis <pierre-louis.landouzi@epita.fr>
2021-07-09 19:56:51 +0200

    commited from ideofhappyness

--- helloWorld/src/main/java/HelloWorld.java
+++ helloWorld/src/main/java/HelloWorld.java
@@ -3,6 +3,11 @@ public class HelloWorld {
     public void printHelloWorld() {
         System.out.print("Hello World!");
     }
+  
+    public void presentation{
+      System.out.print("bonjour");
+    }
+  
 
     static void printHelloWorldErr() {
         System.err.println("Hello World!");
